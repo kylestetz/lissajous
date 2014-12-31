da5e251f603db221cce2761ed9d2b8315f1e36fd
Kyle Stetz <kyle@punkave.com>
2014-12-31 09:29:50 -0500

    added distortion effect

--- src/track.effects.distortion.js
+++ src/track.effects.distortion.js
@@ -1,7 +1,36 @@
-track.prototype.dist = function(amount) {
+track.prototype.dist = function(gain, drive, curve, type) {
   var self = this;
 
-  amount = amount || 1;
+  if(arguments.length == 0) {
+    if(self._distEffectIndex !== null) {
+      // remove the `reset` handler
+      self._off('reset', self._distEffectIndex);
+      self._distEffectIndex = self._removeFromChain(self._distEffectIndex);
+    }
+  } else {
+    if(self._distEffectIndex == null) {
+
+      self._dist = new tuna.Overdrive({
+        outputGain: gain,
+        drive: drive || 0.7,
+        curveAmount: curve || 1,
+        algorithmIndex: type || 0,
+        bypass: 0
+      });
+
+      self._distEffectIndex = self._addToChain(self._dist.input, self._dist);
+
+      // attach a `reset` handler that turns off the effect
+      self._once('reset', self._distEffectIndex, function() {
+        self.dist();
+      });
+    } else {
+      self._dist.outputGain = gain;
+      self._dist.drive = drive || 0.7;
+      self._dist.curveAmount = curve || 1;
+      self._dist.algorithmIndex = type || 0;
+    }
+  }
 
   return self;
 }
